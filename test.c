4b04cd0cea3bbc7496c46bba1e6abd2c222561aa
chriswill88 <williechri79@gmail.com>
2019-07-28 17:05:53 -0400

    test files changed to compare our printf with _printf

--- test.c
+++ test.c
@@ -6,6 +6,16 @@ int main(void)
 	char *d = "bobby";
 	char c = '!';
 
+	int len;
+    int len2;
+
+
+    len = _printf("%s\n", "hey now");
+    len2 = printf("%s\n", "hey now");
+
+    printf("_printf:[%d]\n", len);
+    printf("printf:[%d]\n\n", len2);
+
 	_printf("%shello%%%c%s", d, c, "lol");
 	_printf("%s%s%s", d, d, d);
 
