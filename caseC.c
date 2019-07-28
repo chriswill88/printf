f699cb65406c78ed4e4eeb8f1abffcd22721e5e6
vagrant <vagrant@precise32.(none)>
2019-07-28 22:02:30 +0000

    bettyfix

--- caseC.c
+++ caseC.c
@@ -1,8 +1,12 @@
 #include "holberton.h"
 #include <unistd.h>
-
+/**
+ * caseC - function to write for case c
+ * @v: char
+ * Return: 1 on success
+ */
 int caseC(char v)
 {
 	write(STDOUT_FILENO, &v, 1);
-	return(1);
+	return (1);
 }
