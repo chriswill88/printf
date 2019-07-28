8e570130e80bbb3de34581c886c96822a0139edd
vagrant <vagrant@precise32.(none)>
2019-07-28 21:57:29 +0000

    betty fix: caseC.c caseS.c

--- caseS.c
+++ caseS.c
@@ -1,10 +1,14 @@
 #include "holberton.h"
-
-
+#include <unistd.h>
+/**
+ * caseS - function to write
+ * @z: pointer string
+ * Return: 1
+ */
 int caseS(char *z)
 {
 	int i = 0;
-	
+
 	i = _strlen(z);
 
 	write(STDOUT_FILENO, z, i);
