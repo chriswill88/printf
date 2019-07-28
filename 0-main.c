4f9ede0e06c5e33973a8bbe6a81b512ddc121c37
chriswill88 <williechri79@gmail.com>
2019-07-28 17:57:22 -0400

    0-main betty compliant

--- 0-main.c
+++ 0-main.c
@@ -4,10 +4,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *
- *
- *
- *
+ * _printf - copy of printf function
+ * @format: the operators for the data types to be printed
+ * Return: the number of bytes
  */
 int _printf(const char *format, ...)
 {
@@ -15,13 +14,14 @@ int _printf(const char *format, ...)
 	int x = 0, min = 0, total = 0, strbit = 0;
 	char per;
 	char *s;
+
 	va_start(vl, format);
 	while (format[x] != '\0')
 	{
 		if (format[x] == '%')
 		{
 			min++;
-			switch(format[x + 1])
+			switch (format[x + 1])
 			{
 			case 'c':
 				caseC(va_arg(vl, int));
