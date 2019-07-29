2be7e1bc7dbd29538740b07c8da55fb91e574773
chriswill88 <williechri79@gmail.com>
2019-07-29 12:31:48 -0400

    task 0 main, works for null checks properly

--- 0-main.c
+++ 0-main.c
@@ -4,25 +4,26 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *
- *
- *
- *
+ * _printf - prints any data type to stdout
+ * @format: the string with the operators for the types to be printed
+ * Return: the number of bytes being printed
  */
-
 int _printf(const char *format, ...)
 {
 	va_list vl;
 	int x = 0, min = 0, total = 0, strbit = 0;
 	char per;
 	char *s;
+
+	if (format == NULL || *format == '\0')
+		return (0);
 	va_start(vl, format);
 	while (format[x] != '\0')
 	{
 		if (format[x] == '%')
 		{
 			min++;
-			switch(format[x + 1])
+			switch (format[x + 1])
 			{
 			case 'c':
 				caseC(va_arg(vl, int));
@@ -42,9 +43,7 @@ int _printf(const char *format, ...)
 			x++;
 		}
 		else
-		{
 			write(STDOUT_FILENO, &format[x], 1);
-		}
 		x++;
 	}
 	total = ((x - min) + strbit);
