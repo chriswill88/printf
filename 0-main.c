cd987801f1948ff2ded5f7626697ba7c35b3bb41
chriswill88 <williechri79@gmail.com>
2019-07-30 14:56:35 -0400

    0-main.c

--- 0-main.c
+++ 0-main.c
@@ -1,5 +1,42 @@
 #include "holberton.h"
 /**
+* switchget - finds the operator links it two the function
+* @vl: is the variadic list from main
+* @format: a string that contains operators
+* @x: the index
+* Return: -1 or the number of bytes
+*/
+int switchget(va_list vl, const char *format, int x)
+{
+	char *s, per;
+	int strbit = 0;
+
+	switch (format[x + 1])
+	{
+	case 'c':
+		caseC(va_arg(vl, int));
+		break;
+	case 's':
+		s = va_arg(vl, char*);
+		strbit += caseS(s) - 1;
+		break;
+	case '%':
+		per = '%';
+		write(STDOUT_FILENO, &per, 1);
+		break;
+	case 'd':
+		strbit += caseD(va_arg(vl, int)) - 1;
+		break;
+	case 'i':
+		strbit += caseD(va_arg(vl, int)) - 1;
+		break;
+	default:
+		return (-1);
+	}
+	return (strbit);
+}
+
+/**
  * _printf - prints any data type to stdout
  * @format: the string with the operators for the types to be printed
  * Return: the number of bytes being printed
@@ -8,41 +45,18 @@ int _printf(const char *format, ...)
 {
 	va_list vl;
 	int x = 0, min = 0, total = 0, strbit = 0;
-	char *s, per;
 
 	if (format == NULL)
 		return (-1);
-	if (*format == '\0')
-		return (0);
 	va_start(vl, format);
 	while (format[x] != '\0')
 	{
 		if (format[x] == '%')
 		{
 			min++;
-			switch (format[x + 1])
-			{
-			case 'c':
-				caseC(va_arg(vl, int));
-				break;
-			case 's':
-				min++;
-				s = va_arg(vl, char*);
-				strbit += caseS(s);
-				break;
-			case '%':
-				per = '%';
-				write(STDOUT_FILENO, &per, 1);
-				break;
-			case 'd':
-				caseD(va_arg(vl, int));
-				break;
-			case 'i':
-				caseD(va_arg(vl, int));
-				break;
-			default:
+			strbit = switchget(vl, format, x);
+			if (strbit == -1)
 				return (-1);
-			}
 			x++;
 		}
 		else
